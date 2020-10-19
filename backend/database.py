2e3d8f0be0c697b912e55a03feda28d0b7ed8750
steven <uribgp>
2020-10-18 23:24:14 -0600

    seeds

--- backend/database.py
+++ backend/database.py
@@ -60,14 +60,14 @@ with app.app_context():
 
   video1 = Video(title = 'Third set of 5 squats', description = "This is my third set of five squats.  I felt good on my first four, but the almost bailed on the bar for the fifth.  It felt like my lowerback caved a bit and I had to shrug my shoulders to get the bar up the rest of the way.", link = 'thrallsquat.mp4', owner_id = 555, staff_pick = True, thumbnail = "https://capstone-project-steven-2.s3.us-west-1.amazonaws.com/thrallsquat.png", new_comment = True, total_comments = 3)
   video2 = Video(title = 'Second set of 10 benchpress', description = "This is my second set of 10 for benchress.  I'm currently doing Greyskull linear, and taking this set to failure.  On the last few reps I felt my right hand take over some of the slack of my left, and felt like the bar was coming up unevenly.", link = 'thrallbench.mp4', owner_id = 1, staff_pick = True, thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/thrallbenchthumbnail.png")
-  video3 = Video(title = 'hello', description = "it's me", link = '555/videos/VID_20201008_212621996.mp4', thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/clarencedl.png", owner_id = 3, staff_pick = True)
-  video4 = Video(title = 'hello', description = "it's me", link = '555/videos/VID_20201008_212621996.mp4', thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/clarencepress.png", owner_id = 2, staff_pick = True, total_comments = 1)
-  video5 = Video(title = 'video2', description = "asdfasdf", link = '555/videos/VID_20201008_212621996.mp4', owner_id = 3, thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/lu1.png", new_comment = True)
-  video6 = Video(title = 'hello', description = "'This is my third set of five squats.  I felt good on my first four, but the almost bailed on the bar for the fifth.  It felt like my lowerback caved a bit and I had to shrug my shoulders to get the bar up the rest of the way.'", link = '555/videos/VID_20201008_212621996.mp4', owner_id = 1, staff_pick = True, thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/lu2.png")
-  video7 = Video(title = 'hello', description = "it's me", link = '555/videos/VID_20201008_212621996.mp4', owner_id = 555, staff_pick = True, thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/olympicDL.png")
-  video8 = Video(title = 'hello', description = "it's me", link = '555/videos/VID_20201008_212621996.mp4', thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/squatball.png", owner_id = 3, staff_pick = True, new_comment = True)
-  video9 = Video(title = 'hello', description = "it's me", link = '555/videos/VID_20201008_212621996.mp4', thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/squatjerk.png", owner_id = 4, staff_pick = True)
-  video10 = Video(title = 'video2', description = "asdfasdf", link = '555/videos/VID_20201008_212621996.mp4', owner_id = 2, thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/clarencesquat.png")
+  video3 = Video(title = '5x5 squatjerk', description = "it's me", link = '555/videos/VID_20201008_212621996.mp4', thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/clarencedl.png", owner_id = 3, staff_pick = True)
+  video4 = Video(title = 'last set to failure bench', description = "it's me", link = '555/videos/VID_20201008_212621996.mp4', thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/clarencepress.png", owner_id = 2, staff_pick = True, total_comments = 1)
+  video5 = Video(title = 'Getting pitted max out', description = "asdfasdf", link = '555/videos/VID_20201008_212621996.mp4', owner_id = 3, thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/surferbro.png", new_comment = True)
+  video6 = Video(title = 'final set x 10 cleans', description = "'This is my third set of five squats.  I felt good on my first four, but the almost bailed on the bar for the fifth.  It felt like my lowerback caved a bit and I had to shrug my shoulders to get the bar up the rest of the way.'", link = '555/videos/VID_20201008_212621996.mp4', owner_id = 1, staff_pick = True, thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/lu2.png")
+  video7 = Video(title = 'first set x 5 benchpress', description = "it's me", link = '555/videos/VID_20201008_212621996.mp4', owner_id = 555, staff_pick = True, thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/olympicDL.png")
+  video8 = Video(title = '3 x 5 tex method bench', description = "it's me", link = '555/videos/VID_20201008_212621996.mp4', thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/squatball.png", owner_id = 3, staff_pick = True, new_comment = True)
+  video9 = Video(title = '2/3 set of 5 clean and press', description = "it's me", link = '555/videos/VID_20201008_212621996.mp4', thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/squatjerk.png", owner_id = 4, staff_pick = True)
+  video10 = Video(title = 'OHP max out to failure', description = "asdfasdf", link = '555/videos/VID_20201008_212621996.mp4', owner_id = 2, thumbnail = "https://capstone-project-steven-2.s3-us-west-1.amazonaws.com/clarencesquat.png")
   
   db.session.add(video1)
   db.session.add(video2)
@@ -82,7 +82,7 @@ with app.app_context():
 
   # COMMENTS
 
-  comment1 = Comment(title = 'comment1', text = 'Are your hips beginning to rise while the barbell lags behind?  Will that result in unnecessary strain on your lower back because you end up doing a goodmorning instead of a proper squat?', timestamp = 12, video_id = 1, user_id = 1)
+  comment1 = Comment(title = 'comment1', text = 'Are your hips beginning to rise while the barbell lags behind?  Will that result in unnecessary strain on your lower back because you end up doing a goodmorning instead of a proper squat?', timestamp = 13, video_id = 1, user_id = 1)
   comment2 = Comment(title = 'comment2', text = 'Great Squat, these look way better than last week!', timestamp = None, video_id = 1, user_id = 2)
   comment3 = Comment(title = 'comment3', text = 'some text', timestamp = 3, video_id = 3, user_id = 3)
   comment4 = Comment(title = 'comment4', text = 'some text', timestamp = 2, video_id = 3, user_id = 2)
