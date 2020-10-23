9295972f8bbb63f0806b7d265f2a8deb23143e44
steven <uribgp>
2020-10-23 12:30:44 -0600

    fixed follow routes

--- backend/app/api/follower_routes.py
+++ backend/app/api/follower_routes.py
@@ -1,24 +1,31 @@
 from flask import Blueprint, jsonify, redirect, url_for, session, request
 from app.models import User, db, Follower
 
-follower_routes = Blueprint('followers', __name__)
+follow_routes = Blueprint('followers', __name__)
 
 @follow_routes.route('/', methods=['GET', 'POST', 'DELETE'])
 def follows():
-    current_user = session['user']
+  current_user = session['user']
 
   if request.method == 'POST':
     follow_id = request.json.get("follow")
     follow = Follower (
         follower_by_id=user["id"],
-        following_id=follow
+        following_id=follow_id
     )
     db.session.add(follow)
     db.session.commit()
     return jsonify(msg='follow successful')
+  
   elif request.method == 'DELETE':
     unfollow = request.json.get("unfollow")
-    to_unfollow = Follower.query.get((user_session["id"],unfollow)))
+    to_unfollow = Follower.query.get((user_session["id"],unfollow))
     db.session.delete(to_unfollow)
     db.session.commit()
     return jsonify(msg='unfollowed')
+  
+  elif request.method == 'GET':
+    follows_id = request.json.get("follow")
+    follows = Follower.query.filter(Follower.follower_by_id==follows_id, Follower.verified==True).all()
+    followed_by = Follower.query.filter(Follower.following_id==follows_id, Follower.verified==True).all()
+    followed_by_requests = Follower.query.filter(Follower.following_id==follows_id, Follower.verified==False).all()
